a5ef1f6a3ddc24ccd1527c6181788ee8b7c8b5b3
manav <manavjotsingh17@gmail.com>
2024-10-21 19:29:02 -0700

    changed base url

--- frontend/build/_app/immutable/entry/start.CeY7SZ7U.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D1_TcJ1u.js";export{t as start};
--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://127.0.0.1:80';
+export const BASE_URL = 'http://pyai.duckdns.org';
